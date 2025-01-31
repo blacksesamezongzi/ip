a70138509d787a3cf0ba3554263a0926643bb1ee
blacksesamezongzi <sunjiaen666@gmail.com>
2025-01-31 15:14:59 +0800

    Add storage feature to AdventureGuide

--- src/main/java/AdventureGuide.java
+++ src/main/java/AdventureGuide.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class AdventureGuide {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm adventureGuide");
         System.out.println(" What can I do for you?");
@@ -15,6 +16,24 @@ public class AdventureGuide {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < tasks.length; i++) {
+                    if (tasks[i] != null) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                for (int i = 0; i < tasks.length; i++) {
+                    if (tasks[i] == null) {
+                        tasks[i] = input;
+                        break;
+                    }
+                }
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
             System.out.println("____________________________________________________________");
             System.out.println(" " + input);
